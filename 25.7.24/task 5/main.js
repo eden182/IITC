95a5965893daeb66515487d4f2a9e9d4a9f11b54
eden shabi <eden9876102@gmail.com>
2024-07-27 20:56:10 +0300

    JavaScript Assignment Operators Quiz

---
+++ 25.7.24/task 5/main.js
@@ -0,0 +1,51 @@
+// question 1
+//  =
+
+//  question 2
+//  let name = "eden"
+//  console.log(name);
+
+// question 3
+// += means if you're adding numbers (x += y) === (x = x+y)
+// if you're adding values it will combined them x hello y world
+
+// // question 4
+// let x = 6;
+// console.log((x += 4));
+
+// question 5
+// -= means y - x =
+
+// question 6
+// let y = 10;
+// console.log((y -= 3));
+
+// question 7
+// *= means y * x =
+
+// question 8
+// let z = 4;
+// console.log((z *= 2));
+
+// question 9
+// /= means division y /= x === y / x =
+
+// question 10
+// let w = 6;
+// console.log((w /= 4));
+
+// question 11
+// its the same result but shorter way
+
+// question 12
+// let x = 5
+// console.log(x += 1)
+
+// question 13 14
+// the strings are combined
+// let x = hello
+// console.log( x += world ) // hello world
+
+// // question 15
+// let x = 5;
+// console.log((x += 3)); // 8
