23dc85b9ee7596ca26138bf98053fd3098776660
eden shabi <eden9876102@gmail.com>
2024-07-27 15:33:11 +0300

    # JavaScript Data Types Quiz

---
+++ 25.7.24/task 2/main.js
@@ -0,0 +1,81 @@
+// question 1
+// the data type of x = 5
+// is a primitive number
+
+//question 2
+// you declare a string using ""
+
+//question 3
+//undefine means when you set a variable
+// which you did not gave a value to
+//while null is when an object cannot be created
+
+//question 4
+//you can find out the type of an object
+//using the word typeof
+
+//question 5
+// the data type is a number (primitive)
+
+//question 6
+// the data type of true is boolean (primitive)
+
+//question 7
+// you declare an object using {}
+
+//question 8
+//NaN is not a number
+
+//question 9
+// == means general equlity while === means equlity of the
+// value also
+
+//question 10
+// you can do number() to turn a string
+
+//question 11
+// the data type will be a number
+
+//question 12
+// use typeof (){} to check for a function or object
+
+//question 13
+// you can use template ` $(text) after text defined
+
+//question 14
+// there is no difference between single and double quotes
+
+//question 15
+// you convert a number to string using ""
+
+//question 16
+// typeof null is an object
+
+//question 17
+// using the word const instead of let before the variable
+
+//question 18
+// nothing you need to convert it to a string;
+
+//question 19
+// number
+
+//question 20
+// using backslash \ and ""
+
+//question 21
+// var it's an old use for variables
+// const is a variable that you cannot change later on
+// while let is changeable
+
+//question 22
+// you can use if else method or check with typeof
+
+//question 23
+// it'll write 105 adding like a string
+
+//question 24
+// using the words true or false
+
+//question 25
+// objects are built in data like a function {} or array []
