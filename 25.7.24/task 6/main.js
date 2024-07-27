b1be101778a6a0f7c05bb58f2f6496ca8abeec94
eden shabi <eden9876102@gmail.com>
2024-07-27 21:25:16 +0300

    JavaScript Arithmetic Operations Quiz

---
+++ 25.7.24/task 6/main.js
@@ -0,0 +1,52 @@
+// question 1 2
+// console.log(5 + 3); // 8
+
+// question 3 4
+// (-)
+// console.log(10 - 4); // 6
+
+// question 5 6
+// (*)
+// console.log(6*2); // 12
+
+// question 7 8
+// (/)
+// // console.log(15/3); // 5
+
+// question 9
+// console.log(5 / 0); // infinity
+
+// question 10 11
+// (%)
+// console.log(17 % 5); // 2
+
+// question 12
+// let x = 5;
+// console.log((x += 1));
+
+// question 13
+// same result but shorted
+
+// question 14
+// let y = 5;
+// y--;
+// console.log(y);
+
+// question 15
+// console.log(10 + 5 * 2); // 20
+
+// question 16 17
+// using ()
+// console.log((10 + 5) * 2); // 30
+
+// question 18
+// let x = "5"
+// Number(x)
+
+// question 19
+// let x = "5";
+// console.log(x + 3); // 53
+
+// question 20
+// let y = "10";
+// console.log(y - 5); // 5
