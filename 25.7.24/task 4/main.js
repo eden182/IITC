ace746f5998329e6711f1bf68d7fc92699c612bc
eden shabi <eden9876102@gmail.com>
2024-07-27 20:20:48 +0300

    JavaScript Logical Operators Quiz

---
+++ 25.7.24/task 4/main.js
@@ -0,0 +1,62 @@
+//question 1
+// and = && or = || not = !
+
+//question 2
+// use it to add another parameter for boolean true or false
+
+//question 3
+// true
+
+//question 4
+// false
+
+//question 5
+// use it when you check more then one parameter if one is true
+// then the answer will be true
+
+//question 6
+// true
+
+//question 7
+// false
+
+//question 8
+// if you use it it means that the answer is not what inside the ()
+
+//question 9
+// false
+
+//question 10
+// true
+
+//question 11
+// false
+
+//question 12
+// true
+
+//question 13
+// ! && ||
+
+//question 14
+// is when using && but the first operand is false
+// so the second operand is not evaluated
+
+//question 15
+// when the a is a falsy
+
+//question 16
+// when a is true
+
+//question 17
+// true
+
+//question 18
+// false
+
+//question 19
+// false
+
+//question 20
+// you can use ()
+// !100 + 50*7   ,  (100+50)*7
