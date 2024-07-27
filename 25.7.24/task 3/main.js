d10ab723f563096f155d83b25c648f8b34dae5fa
eden shabi <eden9876102@gmail.com>
2024-07-27 16:26:10 +0300

    JavaScript Comparison Operators Quiz

---
+++ 25.7.24/task 3/main.js
@@ -0,0 +1,38 @@
+//question 1 + 2
+// == general equal === stictly equal to
+
+//question 3
+// !== not equal
+
+//question 4 + 5
+// > is bigger from < is smaller from
+
+//question 6 + 7
+// >=     <=
+
+//question 8
+// true 5 > 3 bigger then
+
+//question 9
+// 10==="10" is false but if it where == it was true
+
+//question 10
+// 7<=7 is true for 7=7
+
+//question 11
+// ===
+
+//question 12
+// true banana have more characters
+
+//question 13
+// !== means not equal to
+
+//question 14
+// true cause "5" is a string which is
+// not equal to a number
+
+//question 15
+// exept adding a string with a number which will
+// work as a string you need to convert most
+// of the data types to make them the same
