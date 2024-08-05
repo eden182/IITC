331767385a7be53e355fcc6b3e2719dbaa187227
eden shabi <eden9876102@gmail.com>
2024-08-05 07:42:34 +0300

    Nested Loop Exercises

--- 1.8.24/nested loops/main.js
+++ 1.8.24/nested loops/main.js
@@ -190,4 +190,120 @@ function odd(h) {
 odd(4);
 
 // question 15
-.
+function abs(g) {
+  let count = 0;
+  for (let i = 0; i < g; i++) {
+    let row = "";
+    for (let j = 0; j < g; j++) {
+      row += Math.abs(j - i) + " ";
+    }
+    console.log(row);
+  }
+}
+abs(3);
+
+// question 16
+let lastLine = "*";
+for (let i = 0; i < 4; i++) {
+  lastLine += "*";
+  let line = "";
+  for (let j = 0; j <= i; j++) {
+    if (j === 0) {
+      line = "*";
+    } else {
+      if (j === i) {
+        line += "*";
+      } else {
+        line += " ";
+      }
+    }
+  }
+  console.log(line);
+}
+console.log(lastLine);
+
+// question 17
+for (let i = 0; i < 4; i++) {
+  let line = "";
+  for (let j = 0; j < 4; j++) {
+    line += Math.min(i, j) + " ";
+  }
+  console.log(line);
+}
+
+// question 18
+for (let i = 0; i < 4; i++) {
+  let line = "";
+  for (let j = 0; j < 4; j++) {
+    line += Math.max(i, j) + " ";
+  }
+  console.log(line);
+}
+
+// question 19
+let currentCharCode = 65;
+for (let i = 0; i < 8; i++) {
+  let line = "";
+  for (let j = 1; j <= i; j++) {
+    line += String.fromCharCode(currentCharCode) + " ";
+    currentCharCode++;
+    if (currentCharCode > 90) {
+      currentCharCode = 65;
+    }
+  }
+  console.log(line);
+}
+
+// question 20
+for (let i = 1; i <= 3; i++) {
+  let line = "";
+  for (let j = 1; j <= 3; j++) {
+    let adding = j * i;
+    let sum = (adding * (adding + 1)) / 2;
+    line += sum + " ";
+  }
+  console.log(line);
+}
+
+// question 21
+//....?....//
+
+// question 22
+function printDiamond(g) {
+  for (let i = g; i <= g; i += 2) {
+    let spaces = (g - i) / 2;
+    console.log(" ".repeat(spaces) + "*".repeat(i));
+  }
+  for (let j = g - 2; j > 0; j -= 2) {
+    let spaces = (g - j) / 2;
+    console.log(" ".repeat(spaces) + "*".repeat(j));
+  }
+}
+
+printDiamond(5);
+
+// question 23
+function Grid(size) {
+  for (let row = 0; row < size; row++) {
+    let rowValues = [];
+    for (let col = 0; col < size; col++) {
+      let value = row ** col;
+      rowValues.push(value);
+    }
+    console.log(rowValues.join(" "));
+  }
+}
+Grid(4);
+
+// question 24
+function Tri(g) {
+  let num = 10;
+  for (let i = 0; i <= g; i++) {
+    let line = "";
+    for (let j = 1; j <= i; j++) {
+      line += " " + num--;
+    }
+    console.log(line);
+  }
+}
+Tri(4);
