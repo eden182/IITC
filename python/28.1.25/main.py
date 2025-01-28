2ed89b5fb8e9217eacd84901756dc566ff57a5cf
eden shabi <eden9876102@gmail.com>
2025-01-28 14:50:14 +0200

    sm

---
+++ python/28.1.25/main.py
@@ -0,0 +1,33 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+import time
+
+service = Service("./chromedriver.exe")
+driver = webdriver.Chrome(service=service)
+options = webdriver.ChromeOptions()
+
+driver.get("http://atid.store")
+driver.maximize_window()
+
+# find a button
+shop_button = driver.find_element(
+    By.XPATH,
+    "//a[@href='https://atid.store/store/']//span[@class='elementor-button-content-wrapper']//span[@class='elementor-button-text'][normalize-space()='Shop Now']"
+)
+
+shop_button.click()
+time.sleep(100)
+
+
+
+
+# driver.get("http://www.google.com")
+# driver.back()
+# time.sleep(2)
+# driver.forward()
+# time.sleep(2)
+# driver.refresh()
+# time.sleep(2)
+# driver.quit()
+# print(driver.title)
