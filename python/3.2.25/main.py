3fb7400bcd8ecc10868384fa62a8e648f5d1ba6f
eden shabi <eden9876102@gmail.com>
2025-02-04 09:58:40 +0200

    sm

---
+++ python/3.2.25/main.py
@@ -0,0 +1,45 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select, WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+from datetime import datetime
+import logging
+
+now_time = datetime.now().strftime("%H-%M-%S")
+
+logging.basicConfig(
+    filename=f"app_test_{now_time}.log",  # Log messages will be written to 'app.log'
+    filemode="w",
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(message)s",
+)
+
+logging.info("Starting Automation Test... ✅")
+service = Service()
+options = webdriver.ChromeOptions()
+options.add_argument("--window-size=2560,1440")
+# options.add_argument('--headless')
+driver = webdriver.Chrome(service=service, options=options)
+
+logging.info("Navigating to the URL... ✅")
+driver.get("https://the-internet.herokuapp.com/javascript_alerts")
+
+try:
+    logging.info("start checking prompt button")
+    pro_button = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.XPATH, "//button[normalize-space()='Click for JS Prompt']"))
+        ).click()
+    logging.info("succses in pro_button✅")
+    alert = driver.switch_to.alert
+    alert.send_keys("baba , dada ,kaka")
+    alert.accept()
+except ArithmeticError as e:
+    logging.error("failed to click pro_button")
+
+
+logging.info("Automation Test Done! ✅")
+
+time.sleep(10)
